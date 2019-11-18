f894225a850421ac2b441bb148aef79c1ae9c029
Philip Panyukov <ppanyukov@googlemail.com>
2019-11-18 12:23:22 +0000

    update atomic_test.go to be more realistic plus better docs
    Signed-off-by: Philip Panyukov <ppanyukov@googlemail.com>

--- atomic_test.go
+++ atomic_test.go
@@ -10,107 +10,68 @@ but without using atomic package.
 
 - loopAtomic: increments shared pointer in the loop using atomic package.
 
-Variable 'routineCount' set the number of goroutines which run in parallel.
-Use 'GOMAXPROCS=N' env var to set the these when running the tests.
-
 It is expected that performance of functions accessing shared pointer will
 degrade with increased number of goroutines due to CPU cache contention and
 invalidation.
 
-Results on my local MBP:
+Results on my local MBP (8x core i9 CPU, hyper-threaded)
 
-	Philips-MBP:go-bench philip$ GOMAXPROCS=1 go test -v -bench=. -benchtime 100x
-	2019/11/18 11:28:13 Number of goroutines: 1
+	Philips-MBP:go-bench philip$ go test -cpu="1,2,4,8,16,24" -bench=. -benchtime=10s
 	goos: darwin
 	goarch: amd64
 	pkg: github.com/ppanyukov/go-bench
-	BenchmarkLoopNoPtr  	     100	   6799168 ns/op
-	BenchmarkLoop       	     100	   6689403 ns/op
-	BenchmarkLoopAtomic 	     100	  10230860 ns/op
-	PASS
-	ok  	github.com/ppanyukov/go-bench	2.407s
+	BenchmarkLoopNoPtr        	    1617	   7278949 ns/op
+	BenchmarkLoopNoPtr-2      	    1564	   7699908 ns/op
+	BenchmarkLoopNoPtr-4      	    1484	   8940401 ns/op
+	BenchmarkLoopNoPtr-8      	    1238	  10630713 ns/op
+	BenchmarkLoopNoPtr-16     	     603	  20165222 ns/op
+	BenchmarkLoopNoPtr-24     	     400	  29864253 ns/op
 
-	Philips-MBP:go-bench philip$ GOMAXPROCS=2 go test -v -bench=. -benchtime 100x
-	2019/11/18 11:28:19 Number of goroutines: 2
-	goos: darwin
-	goarch: amd64
-	pkg: github.com/ppanyukov/go-bench
-	BenchmarkLoopNoPtr-2    	     100	   7133741 ns/op
-	BenchmarkLoop-2         	     100	  25968322 ns/op
-	BenchmarkLoopAtomic-2   	     100	  34951884 ns/op
-	PASS
-	ok  	github.com/ppanyukov/go-bench	6.889s
+	BenchmarkLoop             	    1297	   8095006 ns/op
+	BenchmarkLoop-2           	     462	  26689070 ns/op
+	BenchmarkLoop-4           	     193	  61540155 ns/op
+	BenchmarkLoop-8           	     100	 111132392 ns/op
+	BenchmarkLoop-16          	      80	 144831500 ns/op
+	BenchmarkLoop-24          	      61	 178581242 ns/op
 
-	Philips-MBP:go-bench philip$ GOMAXPROCS=4 go test -v -bench=. -benchtime 100x
-	2019/11/18 11:28:32 Number of goroutines: 4
-	goos: darwin
-	goarch: amd64
-	pkg: github.com/ppanyukov/go-bench
-	BenchmarkLoopNoPtr-4    	     100	   7275700 ns/op
-	BenchmarkLoop-4         	     100	  60480862 ns/op
-	BenchmarkLoopAtomic-4   	     100	  69067172 ns/op
+	BenchmarkLoopAtomic       	    1098	   9719518 ns/op
+	BenchmarkLoopAtomic-2     	     345	  37010750 ns/op
+	BenchmarkLoopAtomic-4     	     175	  68657897 ns/op
+	BenchmarkLoopAtomic-8     	      98	 121333444 ns/op
+	BenchmarkLoopAtomic-16    	      70	 181610799 ns/op
+	BenchmarkLoopAtomic-24    	      55	 219883166 ns/op
 	PASS
-	ok  	github.com/ppanyukov/go-bench	13.853s
+	ok  	github.com/ppanyukov/go-bench	253.595s
 
-	Philips-MBP:go-bench philip$ GOMAXPROCS=8 go test -v -bench=. -benchtime 100x
-	2019/11/18 11:28:57 Number of goroutines: 8
-	goos: darwin
-	goarch: amd64
-	pkg: github.com/ppanyukov/go-bench
-	BenchmarkLoopNoPtr-8    	     100	   8194569 ns/op
-	BenchmarkLoop-8         	     100	 112466232 ns/op
-	BenchmarkLoopAtomic-8   	     100	 123464570 ns/op
-	PASS
-	ok  	github.com/ppanyukov/go-bench	24.651s
-
-	Philips-MBP:go-bench philip$ GOMAXPROCS=16 go test -v -bench=. -benchtime 100x
-	2019/11/18 11:29:34 Number of goroutines: 16
-	goos: darwin
-	goarch: amd64
-	pkg: github.com/ppanyukov/go-bench
-	BenchmarkLoopNoPtr-16     	     100	  15091715 ns/op
-	BenchmarkLoop-16          	     100	 142919302 ns/op
-	BenchmarkLoopAtomic-16    	     100	 184546588 ns/op
-	PASS
-	ok  	github.com/ppanyukov/go-bench	34.631s
-
-	Philips-MBP:go-bench philip$ GOMAXPROCS=24 go test -v -bench=. -benchtime 100x
-	2019/11/18 11:30:17 Number of goroutines: 24
-	goos: darwin
-	goarch: amd64
-	pkg: github.com/ppanyukov/go-bench
-	BenchmarkLoopNoPtr-24     	     100	  24720759 ns/op
-	BenchmarkLoop-24          	     100	 176428833 ns/op
-	BenchmarkLoopAtomic-24    	     100	 236355064 ns/op
-	PASS
-	ok  	github.com/ppanyukov/go-bench	44.192s
+These show rapid degradation in performance when shared pointers get modified.
 
 */
 package go_bench
 
 import (
-	"log"
 	"math"
 	"math/rand"
 	"runtime"
 	"sync"
 	"sync/atomic"
 	"testing"
-	"time"
 )
 
-var routineCount = func() int {
+func routineCount() int {
 	res := runtime.GOMAXPROCS(-1)
-	log.Printf("Number of goroutines: %d\n", res)
 	return res
-}()
+}
 
 var loopCount = 1000000
 
+func newRand() *rand.Rand {
+	return rand.New(rand.NewSource(443))
+}
+
 // loopLocalNoPtr increments local counter and the total counter without using atomic primitives.
 // It increments total counter by taking a local copy first, and adding to it in the end.
-func loopLocalNoPtr(loopCount int, totalCounter *int64) {
-	var random = rand.New(rand.NewSource(time.Now().Unix()))
+func loopLocalNoPtr(loopCount int, totalCounter *int64) int64 {
+	var random = newRand()
 	var localCounter = int64(0)
 	var totalCounterCopy = int64(0)
 	defer func() {
@@ -120,41 +81,48 @@ func loopLocalNoPtr(loopCount int, totalCounter *int64) {
 		localCounter += random.Int63()
 		totalCounterCopy += random.Int63()
 		if localCounter > math.MaxInt64 || totalCounterCopy > math.MaxInt64 {
-			return
+			return localCounter
 		}
 	}
+
+	return localCounter
 }
 
 // loopLocal increments local counter and the total counter without using atomic primitives.
 // It increments total counter directly using pointer dereference.
-func loopLocal(loopCount int, totalCounter *int64) {
-	var random = rand.New(rand.NewSource(time.Now().Unix()))
+func loopLocal(loopCount int, totalCounter *int64) int64 {
+	var random = newRand()
 	var localCounter = int64(0)
 	for i := 0; i <= loopCount; i++ {
 		localCounter += random.Int63()
 		*totalCounter += random.Int63()
 		if localCounter > math.MaxInt64 || *totalCounter > math.MaxInt64 {
-			return
+			return localCounter
 		}
 	}
+
+	return localCounter
 }
 
 // loopLocal increments local counter. It uses atomic primitives to increment total counter.
-func loopAtomic(loopCount int, totalCounter *int64) {
-	var random = rand.New(rand.NewSource(time.Now().Unix()))
+func loopAtomic(loopCount int, totalCounter *int64) int64 {
+	var random = newRand()
 	var localCounter = int64(0)
 	for i := 0; i <= loopCount; i++ {
 		localCounter += random.Int63()
 		totalCounterNew := atomic.AddInt64(totalCounter, random.Int63())
 		if localCounter > math.MaxInt64 || totalCounterNew > math.MaxInt64 {
-			return
+			return localCounter
 		}
 	}
+
+	return localCounter
 }
 
 func BenchmarkLoopNoPtr(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		wg := sync.WaitGroup{}
+		routineCount := routineCount()
 		for r := 0; r < routineCount; r++ {
 			wg.Add(1)
 			totalCounter := int64(0)
@@ -170,6 +138,7 @@ func BenchmarkLoopNoPtr(b *testing.B) {
 func BenchmarkLoop(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		wg := sync.WaitGroup{}
+		routineCount := routineCount()
 		for r := 0; r < routineCount; r++ {
 			wg.Add(1)
 			totalCounter := int64(0)
@@ -185,6 +154,7 @@ func BenchmarkLoop(b *testing.B) {
 func BenchmarkLoopAtomic(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		wg := sync.WaitGroup{}
+		routineCount := routineCount()
 		for r := 0; r < routineCount; r++ {
 			wg.Add(1)
 			totalCounter := int64(0)
